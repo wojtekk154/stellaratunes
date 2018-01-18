5f7b6b0553aa4b5265fa3f1f74f32b9ed030979f
kglowiak <kglowiak@pgs-soft.com>
2018-01-18 10:32:53 +0100

    Added global styles, and header.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,17 @@
 /* eslint-disable no-tabs,no-undef,react/prefer-stateless-function,no-alert,prefer-destructuring */
-import React, { Component } from 'react';
+import React from 'react';
+import './styles.css';
+import logo from './logo.svg';
 
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        Header!
-      </div>
-    );
-  }
+function Header() {
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="logo"/>
+      <h3 className="title">Stellaratunes</h3>
+      <input type="text"/>
+      <button className="btn">test</button>
+    </div>
+  );
 }
 
 export default Header;
